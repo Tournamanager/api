c515359c1155e84798dc1426c901d2aa7a061237
lmtjanssen <lmt.janssen@student.fontys.nl>
2019-10-07 12:56:42 +0200

    Made a first implementation of GraphQL. It is a working example of how it works.

---
+++ src/main/java/com/fontys/api/queries/TeamQuery.java
@@ -0,0 +1,25 @@
+package com.fontys.api.queries;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.fontys.api.entities.Team;
+import com.fontys.api.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class TeamQuery implements GraphQLQueryResolver {
+
+    @Autowired
+    TeamService teamService;
+
+    public List<Team> getTeams(final int count) {
+        return this.teamService.getAllTeams(count);
+    }
+
+    public Optional<Team> getTeam(final Long id) {
+        return this.teamService.getTeam(id);
+    }
+}
