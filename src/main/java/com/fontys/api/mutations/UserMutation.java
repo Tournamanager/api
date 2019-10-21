817f93dea092fdd2a8083b09278d9f2c2f5e8e3c
larsvandriel <lars.van.driel@hotmail.com>
2019-10-21 16:03:12 +0200

    Fixed minor mistakes in UserQuery and UserMutation
    Changed UserMutation so it implements the GraphQLMutationResolver
    Changed UserQuery so it now implements GraphQLQueryResolver instead of GraphQlResolver

--- src/main/java/com/fontys/api/mutations/UserMutation.java
+++ src/main/java/com/fontys/api/mutations/UserMutation.java
@@ -1,15 +1,16 @@
 package com.fontys.api.mutations;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.fontys.api.entities.User;
 import com.fontys.api.service.UserService;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserMutation
+public class UserMutation implements GraphQLMutationResolver
 {
     private final UserService userService;
 
     public UserMutation(UserService userService) { this.userService = userService; }
 
-    public User createUser() { return userService.createUser(); }
+    public User createUser(long id) { return userService.createUser(); }
 }
--- src/main/java/com/fontys/api/queries/UserQuery.java
+++ src/main/java/com/fontys/api/queries/UserQuery.java
@@ -1,5 +1,6 @@
 package com.fontys.api.queries;
 
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.fontys.api.entities.User;
 import com.fontys.api.service.UserService;
@@ -9,13 +10,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class UserQuery implements GraphQLResolver
+public class UserQuery implements GraphQLQueryResolver
 {
     private final UserService userService;
 
     public UserQuery(UserService userService) { this.userService = userService; }
 
-    public List<User> users() { return this.userService.getAllUsers(); }
+    public List<User> users(int count) { return this.userService.getAllUsers(); }
 
     public Optional<User> user(Long id) { return this.userService.getUser(id); }
 }
