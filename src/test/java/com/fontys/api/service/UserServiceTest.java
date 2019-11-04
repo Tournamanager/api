1ced1dcaab17b1749d927f2817d823316e74668c
lmtjanssen <lmtjanssen@users.noreply.github.com>
2019-11-04 12:52:49 +0100

    Updated the tests so that mocking the repositories is no longer done by hardcoding.
    It is now done by using mockito

--- src/test/java/com/fontys/api/service/UserServiceTest.java
+++ src/test/java/com/fontys/api/service/UserServiceTest.java
@@ -1,27 +1,28 @@
 package com.fontys.api.service;
 
-import com.fontys.api.mockrepositories.MockUserRepository2;
+import com.fontys.api.entities.User;
+import com.fontys.api.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
     private UserService userService;
+    private UserRepository userRepositoryMock;
 
     @BeforeEach
     void setUp() {
-        userService = new UserService(new MockUserRepository2());
+        userRepositoryMock = mock(UserRepository.class);
+        userService = new UserService(userRepositoryMock);
     }
 
     @Test
-    void createUserShouldReturnUserUUID() {
-        assertEquals("unique",userService.createUser("unique").getUUID());
-    }
-
-    @Test
-    void createUserShouldReturnOneUser() {
-        userService.createUser("unique");
-        assertEquals(1,userService.getAllUsers().size());
+    void createUserShouldReturnUser() {
+        User u = new User("UUID1");
+        when(userRepositoryMock.save(any(User.class))).thenReturn(u);
+        assertEquals(u,userService.createUser("UUID1"));
+        verify(userRepositoryMock, times(1)).save(u);
     }
 }
