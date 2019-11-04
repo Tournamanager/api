bccac7b268371a0dfa5e39c40139c8951acce131
larsvandriel <lars.van.driel@hotmail.com>
2019-11-04 14:13:18 +0100

    Revert "Revert "Update TournamentService.java""
    This reverts commit 63f1737bbec0a552646ca0cfea6130ca722752f3.

--- src/main/java/com/fontys/api/service/TournamentService.java
+++ src/main/java/com/fontys/api/service/TournamentService.java
@@ -27,23 +27,13 @@ public class TournamentService
     public Tournament createTournament(String name, String description, Integer ownerId, int numberOfTeams)
     throws InvalidAttributeValueException
     {
-        if (numberOfTeams <= 1)
-        {
-            throw new InvalidAttributeValueException(String.format(
-                    "A tournament must be created for at least 2 teams. Number of teams provided was %d." +
-                    " Please change the value and try again.", numberOfTeams));
-        }
-        if (name == null || name.isBlank())
-        {
-            throw new InvalidAttributeValueException(
-                    "The tournament name can't be empty. Please give your tournament a name and try again.");
-        }
-        if (ownerId == null || ownerId < 1)
-        {
-            throw new InvalidAttributeValueException(
-                    "Something went wrong while creating the tournament. Please try again.");
-        }
+        validateTournamentName(name);
+        validateUserId(ownerId);
+        validateNumberOfTeams(numberOfTeams);
+
         User user = userRepository.findById(ownerId).orElse(null);
+        validateOwner(user);
+
         return tournamentRepository.save(new Tournament(name, description, user, numberOfTeams));
     }
 
@@ -57,5 +47,41 @@ public class TournamentService
     public Optional<Tournament> tournament(Integer id)
     {
         return tournamentRepository.findById(id);
+    }
+
+    private void validateTournamentName(String name) throws InvalidAttributeValueException
+    {
+        if (name == null || name.isBlank())
+        {
+            throw new InvalidAttributeValueException(
+                    "The tournament name can't be empty. Please give your tournament a name and try again.");
+        }
+    }
+
+    private void validateUserId(Integer ownerId) throws InvalidAttributeValueException
+    {
+        if (ownerId == null || ownerId < 1)
+        {
+            throw new InvalidAttributeValueException(
+                    "Something went wrong while creating the tournament. Please try again.");
+        }
+    }
+
+    private void validateNumberOfTeams(Integer numberOfTeams) throws InvalidAttributeValueException
+    {
+        if (numberOfTeams <= 1)
+        {
+            throw new InvalidAttributeValueException(String.format(
+                    "A tournament must be created for at least 2 teams. Number of teams provided was %d." +
+                    " Please change the value and try again.", numberOfTeams));
+        }
+    }
+
+    private void validateOwner(User owner) throws InvalidAttributeValueException
+    {
+        if (owner == null)
+        {
+            throw new InvalidAttributeValueException("An error occurred while creating the tournament. The user was not found! Please try again.");
+        }
     }
 }
