91987a527cd1ff22a6e2bddb6f22f63adbf98813
lfb0801 <lfb0801@gmail.com>
2019-11-11 11:14:56 +0100

    added update function and reformatted files

--- src/main/java/com/fontys/api/mutations/TournamentMutation.java
+++ src/main/java/com/fontys/api/mutations/TournamentMutation.java
@@ -8,16 +8,21 @@ import org.springframework.stereotype.Component;
 import javax.naming.directory.InvalidAttributeValueException;
 
 @Component
-public class TournamentMutation implements GraphQLMutationResolver
-{
+public class TournamentMutation implements GraphQLMutationResolver {
     private final TournamentService tournamentService;
 
-    public TournamentMutation(TournamentService tournamentService) { this.tournamentService = tournamentService; }
+    public TournamentMutation(TournamentService tournamentService) {
+        this.tournamentService = tournamentService;
+    }
 
     public Tournament createTournament(String name, String description, Integer ownerId, Integer numberOfTeams)
-    throws InvalidAttributeValueException
-    {
+            throws InvalidAttributeValueException {
         return tournamentService.createTournament(name, description, ownerId, numberOfTeams);
+    }
+
+    public Tournament updateTournament(Integer id, String name, String description, Integer ownerId, Integer numberOfTeams)
+            throws InvalidAttributeValueException {
+        return tournamentService.updateTournament(id, name, description, ownerId, numberOfTeams);
     }
 
     public String deleteTournament(Integer id) {
