9285f7a7b4fbd60ef2f7d42d3d44767d54b5fd00
larsvandriel <lars.van.driel@hotmail.com>
2019-11-12 17:45:47 +0100

    Added Exceptions to method signature in Mutations

--- src/main/java/com/fontys/api/mutations/MatchMutation.java
+++ src/main/java/com/fontys/api/mutations/MatchMutation.java
@@ -3,6 +3,9 @@ package com.fontys.api.mutations;
 import com.fontys.api.entities.Match;
 import com.fontys.api.service.MatchService;
 
+import javax.naming.directory.InvalidAttributeValueException;
+import java.text.ParseException;
+
 public class MatchMutation
 {
     private final MatchService matchService;
@@ -13,6 +16,7 @@ public class MatchMutation
     }
 
     public Match createMatch(Integer teamHomeId, Integer teamAwayId, String date)
+    throws ParseException, InvalidAttributeValueException
     {
         return matchService.createMatch(teamHomeId, teamAwayId, date);
     }
